2fb0a7c754a9bcaa726e1600d66e61862b2949db
JesuFrancesco <117495071+JesuFrancesco@users.noreply.github.com>
2025-07-18 17:47:18 -0500

    chore(data): agregar mas tablas al chatbot

--- packages/oracle-setup/scripts/2.ora_ai_create_profile.sql
+++ packages/oracle-setup/scripts/2.ora_ai_create_profile.sql
@@ -8,7 +8,22 @@ BEGIN
             "provider": "oci",
             "credential_name": "MI_OCIAI_CRED",
             "object_list": [
-                {"owner": "BANCOLIMA", "name": "employees"}
+                {"owner": "BANCOLIMA", "name": "employees"},
+                {"owner": "BANCOLIMA", "name": "cambio_divisas"},
+                {"owner": "BANCOLIMA", "name": "cliente_cuentas"},
+                {"owner": "BANCOLIMA", "name": "cliente_planespension"},
+                {"owner": "BANCOLIMA", "name": "clientes"},
+                {"owner": "BANCOLIMA", "name": "cuentas"},
+                {"owner": "BANCOLIMA", "name": "depositos"},
+                {"owner": "BANCOLIMA", "name": "fianzas"},
+                {"owner": "BANCOLIMA", "name": "fondos_inversion"},
+                {"owner": "BANCOLIMA", "name": "hipotecas"},
+                {"owner": "BANCOLIMA", "name": "leasing"},
+                {"owner": "BANCOLIMA", "name": "pago_servicios"},
+                {"owner": "BANCOLIMA", "name": "planes_pension"},
+                {"owner": "BANCOLIMA", "name": "prestamos"},
+                {"owner": "BANCOLIMA", "name": "tarjetas_credito"},
+                {"owner": "BANCOLIMA", "name": "transferencias"}
             ],
             "model": "meta.llama-4-scout-17b-16e-instruct"
         }'
